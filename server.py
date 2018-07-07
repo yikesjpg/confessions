1bb832add09e1bde1c330e092a4e8d1c209b4b7a
dcao <dcao@protonmail.com>
2018-07-07 00:34:59 -0700

    redis :(

--- server.py
+++ server.py
@@ -13,14 +13,16 @@ from apscheduler.triggers.cron import CronTrigger
 import atexit
 import facebook
 from flask import Flask, jsonify, request
+import json
 import os
 from pytz import timezone
+import redis
 
 app = Flask(__name__)
+db  = redis.Redis(os.environ["REDIS_URL"])
 
-st = {
-    "postq": []
-}
+if not db.exists("postq"):
+    db.set("postq", [])
 
 def post(s):
     access_token = os.environ["ACCESS_TOKEN"]
@@ -30,20 +32,16 @@ def post(s):
         message=s)
 
 def popq():
-    global st
-
-    if st["postq"]:
-        post(st["postq"].pop(0))
+    if db.get("postq"):
+        post(db.lpop("postq"))
 
 @app.route('/posts', methods=["GET", "POST"])
 def posts():
-    global st
-
     if request.method == "POST":
-        st["postq"] += request.get_json()
+        db.rpush("postq", *request.get_json())
         return "it g ma"
     else:
-        return jsonify(postq)
+        return jsonify(db.get("postq"))
 
 @app.route('/state', methods=["GET", "POST"])
 def state():
@@ -52,10 +50,12 @@ def state():
     if request.method == "POST":
         json = request.get_json()
         for k in json:
-            state[k] = v
-        return jsonify(st)
-    else:
-        return jsonify(st)
+            db.set(k, json[k])
+
+    st = {
+        "postq": db.get("postq")
+    }
+    return jsonify(st)
 
 scheduler = BackgroundScheduler(timezone=timezone("America/Los_Angeles"))
 scheduler.start()
